2dad119d82f262773476ab4571ee1e76bc4929e4
d G <dggtn97@gmail.com>
2024-04-07 13:23:51 -0300

    ejercicios hasta el tp5

---
+++ tp5.py
@@ -0,0 +1,66 @@
+print("TP5")
+""" Ejercicio 1: Leer números que representan edades de un grupo de personas, finalizando la
+lectura cuando se ingrese el número -1. Imprimir cuántos son menores de 18
+años, cuántos tienen 18 años o más y el promedio de edad de ambos grupos.
+Descartar valores que no representan una edad válida. (Se considera válida una
+edad entre 0 y 100).
+Ejercicio 2: Escribir un programa que permita ingresar los números de legajo de los alumnos
+de un curso y su nota de examen final. El fin de la carga se determina ingresando
+un -1 en el legajo. Informar para cada alumno si aprobó o no el examen considerando
+que se aprueba con nota mayor o igual a 4. Se debe validar que la
+nota ingresada sea entre 1 y 10. Terminada la carga de datos, informar:
+· Cantidad de alumnos que aprobaron con nota mayor o igual a 4.
+· Cantidad de alumnos que desaprobaron el examen con nota menor a 4.
+· Porcentaje de alumnos que están aplazados (tienen 1 en el examen).
+Ejercicio 3: Una empresa aplica el siguiente procedimiento en la comercialización de sus productos:
+· Aplica el precio base a la primera docena de unidades.
+· Aplica un 10% de descuento a todas las unidades entre 13 y 100.
+· Aplica un 25% de descuento a todas las unidades por encima de las 100.
+Por ejemplo, supongamos que vende 230 unidades de un producto cuyo precio
+base es 100. El cálculo resultante sería:
+100 * 12 + 90 * 88 + 75 * 130 = 18870
+y el precio promedio es de 18870/230 = 82.04
+Escribir un programa que lea la cantidad solicitada de un producto y su precio
+base, y muestre el valor total de la venta y el precio promedio por unidad. El fin
+de carga se determina ingresando -1 como cantidad solicitada. Al finalizar informar:
+· Cantidad de ventas realizadas total.
+· Cantidad de ventas en las que se aplicó un 10% de descuento.
+· Cantidad de ventas en las que SOLO se aplicó el precio base, es decir
+que no se le realizaron descuentos.
+Ejercicio 4: Una empresa factura a sus clientes el último día de cada mes. Si el cliente paga
+su factura dentro de los primeros 10 días del mes siguiente, tiene un descuento
+de $200 o del 2% de la factura, lo que resulte más conveniente para el cliente.
+Si paga en los siguientes 10 días del mes deberá pagar el importe original de la
+factura, mientras que si paga después del día 20 deberá abonar una multa de
+$350 o del 10% de su factura, lo que resulte mayor. Escribir un programa que
+lea el número del cliente y el total de la factura, y emita un informe donde conste
+el número del cliente y los tres importes que podría abonar según la fecha de
+pago.
+Ejercicio 5: Leer tres números D, M y A correspondientes al día, mes y año de una fecha, y
+un número entero N que representa una cantidad de días. Realizar un programa
+que imprima la nueva fecha que resulta de sumar N días a la fecha dada. Tener
+en cuenta los años bisiestos tal como se detalla en el ejercicio 7 de la práctica 3.
+Ejercicio 6: Leer un número entero y mostrar un mensaje informando cuántos dígitos contiene.
+Tener en cuenta que el número puede ser negativo. Ejemplo: Si se ingresa
+12345 se debe imprimir 5.
+Ejercicio 7: Leer un número entero e invertir las cifras que contiene. Imprimir por pantalla el
+número invertido. Tener en cuenta que el número puede ser negativo. Por ejemplo,
+si se ingresa 1234 debe mostrar 4321.
+Ejercicio 8: Una empresa cuenta con N empleados, divididos en tres categorías (1, 2 y 3).
+Por cada empleado se lee su legajo, categoría y salario. Se solicita elaborar un
+informe que contenga:
+· Importe total de salarios pagados por la empresa.
+· Cantidad de empleados que ganan más de $200000.
+· Cantidad de empleados que ganan menos de $50000, cuya categoría sea
+3.
+· Legajo del empleado que más gana.
+· Sueldo más bajo.
+· Importe total de sueldos por cada categoría.
+· Salario promedio
+Ejercicio 9: Ingresar un número N y validar que sea positivo. Luego:
+a. Mostrar los primeros números impares, hasta alcanzar el número ingresado.
+b. Informar la suma de esos números impares.
+Ejemplo:
+· Si se ingresa 5, se debe mostrar 1 3 5 y la suma es 9.
+· Si se ingresa 8, se debe mostrar 1 3 5 7 y la suma es 16.
+· Si se ingresa -5, se debe pedir otro número."""
