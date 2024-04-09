4d0fab0f28cb21688d29bdecbc800c63c63f3176
dggtn9 <dggtn97@gmail.com>
2024-04-09 16:58:26 -0300

    ejercicios de cada tp en cada carpeta

---
+++ TP2/2.py
@@ -0,0 +1,9 @@
+"""Ejercicio 2: Desarrollar un programa que permita ingresar dos números enteros A y
+B a través del teclado. Imprimir su suma y su diferencia."""
+print("EJERCICIO 2 :")
+numeroUno=int(input("Ingrese un numero entero"))
+numeroDos=int(input("Ingrese un numero entero"))
+resta= numeroUno-numeroDos
+suma=numeroUno+numeroDos
+print("el resultado de la suma es:",suma)
+print("el resultado de la resta es:",resta)
---
+++ TP2/3.py
@@ -0,0 +1,9 @@
+"""Ejercicio 3: Calcular el promedio de las notas que obtiene un alumno al rendir los dos
+parciales."""
+print("EJERCICIO 3 :")
+parcial1=int(input("Ingresa tu nota del parcial 1"))
+parcial2=int(input("Ingresa tu nota del parcial 2"))
+promedio = (parcial2+parcial1)/2
+print("Promedio de tus notas es: ",promedio)
+
+
